1f262b58546d526afdf7498f47ee6a3ea3f84b8e
useyournose <mroliverberger@gmx.de>
2025-02-22 01:30:19 +0100

    pwa

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,17 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  // Get all "navbar-burger" elements
+  (document.querySelectorAll('.navbar-burger') || []).forEach( el => {
+    el.addEventListener('click', () => {
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+    });
+  });
   
 });
 
