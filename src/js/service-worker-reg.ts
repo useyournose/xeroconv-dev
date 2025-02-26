ca0d39f59e53c014946495913c04d42b74ed4265
useyournose <mroliverberger@gmx.de>
2025-02-26 20:33:57 +0100

    pwa

--- src/js/service-worker-reg.ts
+++ src/js/service-worker-reg.ts
@@ -1,6 +1,7 @@
-const sw = "js/service-worker.js"; // it is needed because parcel will not recognize this as a file and not precess in its manner
-navigator.serviceWorker
-    .register(sw, {type:'module'})
+navigator.serviceWorker.register(new URL("./service-worker.js", import.meta.url), {type:'module'})
+////const sw = "js/service-worker.js"; // it is needed because parcel will not recognize this as a file and not precess in its manner
+//navigator.serviceWorker
+    //.register(sw, {type:'module'})
     .then(registration => {
     registration.onupdatefound = () => {
         const installingWorker = registration.installing;
