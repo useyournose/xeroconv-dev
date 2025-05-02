a3d41af79b95d448a4fb78df652949b29bf0c091
useyournose <mroliverberger@gmx.de>
2025-05-02 08:51:37 +0200

    pwa

--- src/js/downloadhandler.ts
+++ src/js/downloadhandler.ts
@@ -1,27 +1,30 @@
-import { showSuccess } from "./messages";
-import { openModal } from "./modals";
+import { showSuccess,showError } from "./messages";
+import { closeModal, openModal } from "./modals";
 import downloadFiles  from "./downloadFiles";
 
 export default function download(files:File[]):Promise<boolean|string> {
   return new Promise((resolve,reject) => {
-    if (!(navigator.share === undefined) && navigator.canShare({files})) {
+    if (!navigator.canShare && navigator.canShare({files})) {
       console.log('Sharing files is supported');
       const target = document.getElementById('modal-download');
       const shareButton = document.getElementById('dl-share');
       const dlButton = document.getElementById('dl-store');
       shareButton.addEventListener('click', async () => {
         try{
-          await navigator.share({title : "Open Files", text : "with your preferred app", files : files})
+          await navigator.share({title : "Open Files", text : "with your preferred app", files})
           console.log("[download]: file saved.");
           showSuccess("Saved via fileshare.");
           resolve(true)
+          closeModal(target)
         } catch (err) {
+          showError(`The file could not be shared: ${err}`)
           console.error(`The file could not be shared: ${err}`);
         }
       })
         
       dlButton.addEventListener('click', () => {
         downloadFiles(files)
+        closeModal(target)
       })
       openModal(target);
     } else {
