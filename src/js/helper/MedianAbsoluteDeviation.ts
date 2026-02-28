668e6fe73ce8ef439ef0085c2a01607dd6f6dead
useyournose <mroliverberger@gmx.de>
2026-02-28 21:06:05 +0100

    11

--- src/js/helper/MedianAbsoluteDeviation.ts
+++ src/js/helper/MedianAbsoluteDeviation.ts
@@ -1,4 +1,4 @@
-import { quantile } from "./quantile";
+import { quantile } from "./Quantile";
 export function medianAbsoluteDeviation(sorted: number[]) {
     const med = quantile(sorted, 0.5);
     const devs = sorted.map(v => Math.abs(v - med)).sort((a, b) => a - b);
